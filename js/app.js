2b1df8ffd5be5579db7c9303bfa32f46709c7b2b
timurprimbetov <itcenterqaraozek22@gmail.com>
2022-08-26 17:00:51 +0500

    Add site

---
+++ js/app.js
@@ -0,0 +1,8 @@
+window.AddEventListner("scroll", function (){ 
+	var navScroll = document.getElementById('nav');
+	navScroll.classList.toggle("fixed", window.scrollY > 5);
+});
+
+function navBack(){
+	var nav = document.getElementById('nav').classList.toggle("change");
+}
